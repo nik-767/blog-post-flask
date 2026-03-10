7cd298ba839f3c0ded87dc77adbc2193af03f992
nikhil <nikhilkalra124421@email.com>
2026-03-10 13:09:10 +0530

    updating blog login

---
+++ app.py
@@ -0,0 +1,89 @@
+
+from flask import Flask, render_template, session, request, Response, redirect, url_for 
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "mysecretkey"
+
+
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(200), nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    date_created = db.Column(db.DateTime, default=db.func.now())
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(200), nullable=False)
+
+    email = db.Column(db.String(150), nullable=False)
+
+    password = db.Column(db.String(200), nullable=False)
+
+    date_created = db.Column(db.DateTime, default=db.func.now())
+
+
+
+@app.route("/")
+def home():
+    return render_template("register.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        new_user = User(name=name, email=email, password=password )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("login.html")
+    else:
+        return render_template("register.html")
+    
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password == password:
+            session["user_id"] = user.id
+            return redirect(url_for("home"))
+    else:
+        return render_template("login.html")
+
+
+
+
+
+
+
+
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
